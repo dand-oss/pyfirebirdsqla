6ff714645efc3013d76c2128d2f2f55dd6415dfe
Dan Dees <dand@appsmiths.com>
2021-01-14 15:36:09 +0700

    revert to Nov 16th

--- src/firebirdsqla/__init__.py
+++ src/firebirdsqla/__init__.py
@@ -1,24 +1,18 @@
-"""
-Connection Shim
-"""
-from firebirdsql import Connection
-from tpying import TypeVar
-
-conn_T = TypeVar("T", bound="ConnectionShim")
+from firebirdsql import *
 
 
 class ConnectionShim(Connection):
-    def __init__(self, host: str = "", **kargs):
-        if "/" in host:
-            host = host[0 : host.find("/")]
+    def __init__(self, host="", **kargs):
+        if '/' in host:
+            host = host[0:host.find('/')]
         super().__init__(host=host, **kargs)
 
 
-def connect(**kwargs) -> conn_T:
+def connect(**kwargs):
     conn = ConnectionShim(**kwargs)
     return conn
 
 
-def create_database(**kwargs) -> conn_T:
-    kwargs["create_new"] = True
+def create_database(**kwargs):
+    kwargs['create_new'] = True
     return connect(**kwargs)
