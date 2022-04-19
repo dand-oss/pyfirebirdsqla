f576ee837f132e4f11d38ee2444d63d413e0ed86
Dan Dees <dand@appsmiths.com>
2022-04-19 14:26:26 +0700

    code

--- src/firebirdsqla/__init__.py
+++ src/firebirdsqla/__init__.py
@@ -1,25 +1,29 @@
 """
 Connection Shim
 """
+import typing
+
 import firebirdsql as fb
 from firebirdsql import *  # fails with this!!! code run at import time?
-from typing import TypeVar
-
-conn_T = TypeVar("T", bound="ConnectionShim")
 
 
 class ConnectionShim(fb.Connection):
-    def __init__(self, host: str = "", **kargs):
+    """Shim firebird connection."""
+
+    def __init__(self, host: str = "", **kargs: str):
+        """Override initialize instance."""
         if "/" in host:
             host = host[0 : host.find("/")]
         super().__init__(host=host, **kargs)
 
 
-def connect(**kwargs) -> conn_T:
+def connect(**kwargs: str) -> ConnectionShim:
+    """Override db connect."""
     conn = ConnectionShim(**kwargs)
     return conn
 
 
-def create_database(**kwargs) -> conn_T:
+def create_database(**kwargs: typing.Any) -> ConnectionShim:
+    """Call from sqlalchemy."""
     kwargs["create_new"] = True
     return connect(**kwargs)
--- src/firebirdsqla/compat.py
+++ src/firebirdsqla/compat.py
@@ -1,19 +1,24 @@
-"""
-patches pyfirebirdsql for sqalalchemy
-
-"""
+"""Patches pyfirebirdsql for sqlalchemy."""
 import sys
+import typing
+
 import firebirdsqla
 
 # function used to parse dsn
-_f_parse_dsn = None
+g_f_parse_dsn: typing.Callable = None
 
 
-def sqla_parse_dsn(dsn, host=None, port=None, database=None, user=None, password=None):
-    global _f_parse_dsn
-
-    if not _f_parse_dsn:
-        raise "Must register first"
+def sqla_parse_dsn(
+    dsn: str,
+    host: str = None,
+    port: str = None,
+    database: str = None,
+    user: str = None,
+    password: str = None,
+) -> str:
+    """Parse DSN for sqlalchemy."""
+    if not g_f_parse_dsn:
+        raise RuntimeError("Must register first")
 
     # check the driver version
     least_vers = "1.1.3"
@@ -21,17 +26,18 @@ def sqla_parse_dsn(dsn, host=None, port=None, database=None, user=None, password
 
     fb_vers = fbm.__version__
     if fb_vers <= least_vers:
-        raise f"Require firebirdsql driver > {least_vers}"
+        raise RuntimeError(f"Require firebirdsql driver > {least_vers}")
 
     # two lines to fix SQLA
     if "/" in host:
         host = host[0 : host.find("/")]
 
     # run the "original"
-    return _f_parse_dsn(dsn, host, port, database, user, password)
+    return g_f_parse_dsn(dsn, host, port, database, user, password)
 
 
-def register():
+def register() -> None:
+    """Register this driver as fdb."""
     # WE are the fdb driver!
     sys.modules["fdb"] = firebirdsqla
 
@@ -39,10 +45,9 @@ def register():
     # upstream refused the fix...
     from firebirdsql import utils
 
-    global _f_parse_dsn
-
     # out with the old
-    _f_parse_dsn = utils.parse_dsn
+    global g_f_parse_dsn
+    g_f_parse_dsn = utils.parse_dsn
 
     # in with the new
     utils.parse_dsn = sqla_parse_dsn
